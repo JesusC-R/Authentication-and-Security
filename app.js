2980c967ab0add21b5664bd653f62373cd949e4a
Jesus Cuevas-Rodarte <cuevasrodartej@gmail.com>
2023-02-17 17:12:42 -0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,101 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const {Schema} = require("mongoose");
+
+const {createNullProtoObjWherePossible} = require("ejs/lib/utils");
+
+const app = express();
+
+mongoose.set("strictQuery", false);
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+main().catch(err => console.log(err));
+
+async function main(){
+    const DataBaseName = "SecretsDB";
+
+    await mongoose.connect("mongodb://127.0.0.1:27017/" + DataBaseName, {useNewUrlParser: true});
+
+    const userSchema = new Schema({
+        email: String,
+        password: String
+    });
+
+    const User = mongoose.model("User", userSchema);
+
+    User.find((err, data) => {
+        if(!err){
+            console.log(data);
+        }
+        else{
+            console.log(err);
+        }
+    });
+
+
+
+
+    app.get("/", (req, res) => {
+        res.render("home");
+    });
+
+    app.get("/login", (req, res) => {
+        res.render("login");
+    });
+
+    app.get("/register", (req, res) => {
+        res.render("register");
+    })
+
+
+    app.post("/register", (req, res) => {
+        const userEmail = req.body.username;
+        const userPassword = req.body.password;
+
+        const newUser = new User({
+            email: userEmail,
+            password: userPassword
+        });
+        newUser.save(err => {
+            if(!err){
+                res.render("secrets");
+            }
+            else{
+                console.log(err);
+            }
+        });
+    });
+
+    app.post("/login", (req, res) => {
+        const userEmail = req.body.username;
+        const userPassword = req.body.password;
+
+        User.findOne({email: userEmail}, (err, userFound) => {
+            if(!err){
+                if(userFound) {
+                    if (userPassword === userFound.password) {
+                        res.render("secrets");
+                    } else {
+                        console.log("Incorrect Password");
+                    }
+                }
+                else{
+                    console.log("User Not Found");
+                }
+            }
+            else{
+                console.log(err);
+            }
+        });
+    });
+
+
+    app.listen(3000, () => {
+        console.log("Running on port 3000");
+    });
+}
